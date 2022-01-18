c842f9fec5ba4171b86c276877aa6bcc65c23861
Hugo Villeneuve <hvilleneuve@dimonoff.com>
2022-01-18 15:38:05 -0500

    [TO SUBMIT] rtc: pcf2127: add error message for incompatible I2C adapter
    pcf2127_i2c_probe() may fail silently if the I2C adapter functionality
    is incompatible with our driver requirements. If this is the case, display
    an error message.
    
    Signed-off-by: Hugo Villeneuve <hvilleneuve@dimonoff.com>

--- drivers/rtc/rtc-pcf2127.c
+++ drivers/rtc/rtc-pcf2127.c
@@ -1538,8 +1538,10 @@ static int pcf2127_i2c_probe(struct i2c_client *client)
 	};
 	const struct pcf21xx_config *variant;
 
-	if (!i2c_check_functionality(client->adapter, I2C_FUNC_I2C))
+	if (!i2c_check_functionality(client->adapter, I2C_FUNC_I2C)) {
+		dev_err(&client->dev, "Adapter does not support I2C_FUNC_I2C\n");
 		return -ENODEV;
+	}
 
 	if (client->dev.of_node) {
 		variant = of_device_get_match_data(&client->dev);
