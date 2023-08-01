86aa58851d7ef7617c41c2b84f7ade2c2837ed26
Hugo Villeneuve <hvilleneuve@dimonoff.com>
2023-08-01 15:35:03 -0400

    end pcf2131-2

--- patches/pcf2131-2/infos.sh
+++ patches/pcf2131-2/infos.sh
@@ -1,11 +1,11 @@
-series="1"
+series="2"
 subject="rtc: pcf2127: add default battery-related power-on values"
 
 base_branch="abelloni/rtc-next"
 compile_branch="abelloni/rtc-next"
 
 commit_start="start pcf2131-2"
-commit_end="rtc: pcf2127: add support for new DT battery properties"
+commit_end="rtc: pcf2127: add support for battery-related DT properties"
 
 # Additional CC entries (must be space separated):
 cc_list="hugo@hugovil.com bruno.thomsen@gmail.com"
